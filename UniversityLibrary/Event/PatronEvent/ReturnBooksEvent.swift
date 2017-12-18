695eb396e277df4fe058fe3567bec88439484e26
huyvo <huyvanvo94@gmail.com>
2017-12-17 16:45:17 -0800

    update

--- UniversityLibrary/Event/PatronEvent/ReturnBooksEvent.swift
+++ UniversityLibrary/Event/PatronEvent/ReturnBooksEvent.swift
@@ -156,7 +156,7 @@ class ReturnBooksEvent: AbstractEvent{
                             }
                             
                         }else{
-                           
+                            
                         }
                         
                         self.doReturn()
